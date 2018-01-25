62cbd28a9ab31ee61fa1fce570c7c1ae13c390c8
Manjusha Z <manjushaz2012@gmail.com>
2018-01-25 19:08:59 +0530

    Undoing the previous commit

--- microservices/ui/app/src/index.js
+++ microservices/ui/app/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import HasuraExampleApp from './hasuraExamples/HasuraExampleApp';
+
+//Replace HasuraExampleApp with your own component
+ReactDOM.render(
+  <HasuraExampleApp />,
+  document.getElementById('root')
+);
