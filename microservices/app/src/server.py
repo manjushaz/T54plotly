3d810d8b53debf51982299441c79436e2c42c202
Manjusha Z <manjushaz2012@gmail.com>
2018-02-10 19:01:45 +0530

    Added /happiness to server.py

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -1,5 +1,10 @@
 from src import app
 # from flask import jsonify
+from flask import Flask, render_template,jsonify, redirect, url_for, request ,make_response,abort
+from flask_cors import CORS
+import requests
+import json
+import os
 
 
 @app.route("/")
@@ -11,3 +16,26 @@ def home():
 # @app.route("/json")
 # def json_message():
 #     return jsonify(message="Hello World")
+
+@app.route('/happiness')
+def happiness():
+    url = "https://data.chestnut27.hasura-app.io/v1/query"
+    requestPayload = {
+        "type": "select",
+        "args": {
+            "table": "happinessrepo",
+            "columns": [
+                "*"
+            ]
+        }
+    }
+    # Setting headers
+   
+    headers = {
+        "Content-Type": "application/json"
+    }
+    # Make the query and store response in resp
+    resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
+    # resp.content contains the json response.
+    return resp.content
+
